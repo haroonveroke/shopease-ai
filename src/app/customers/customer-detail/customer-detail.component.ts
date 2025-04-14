cbc1484f9ee5e05dce5472dd29a18277b7c6e3d3
Haroon Abid <haroon.abid@veroke.com>
2025-04-14 16:30:34 +0500

    modify customer detail UI

--- src/app/customers/customer-detail/customer-detail.component.ts
+++ src/app/customers/customer-detail/customer-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,6 +12,7 @@ import { Observable } from 'rxjs';
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule,
     MatCardModule,
     MatButtonModule,
     MatIconModule
